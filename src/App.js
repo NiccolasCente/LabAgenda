8235dc4b199e448894f020647c213a5d85383d20
NiccolasCente <niccolascente@hotmail.com>
2024-03-15 20:37:06 -0300

    Projeto Aplicado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import img1 from "./assets/img1.png";
+
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // requisição HTTP
+    fetch('URL_DO_SEU_ENDPOINT', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+    })
+    .then(response => {
+      if (response.ok) {
+        // Se a requisição for bem-sucedida, realizar alguma ação, como redirecionar o usuário para outra página
+       
+         //window.location.href = creation_account.js;
+      } else {
+        // Se a requisição não for bem-sucedida, exibir uma mensagem de erro
+        
+      }
+    })
+    .catch(error => {
+     
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+        <img className="logo" src={img1} alt="logo FSA" />
+          <form className="login-form">
+            <span className="login-form-title"> Bem vindo </span>
+
+            <span className="login-form-title">
+             
+            </span>
+
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Senha"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn" onClick={handleLogin}>Login</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
